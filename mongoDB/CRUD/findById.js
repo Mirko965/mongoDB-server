5423b046e00679352a33d7114eb564f78fbb11e7
Mirko965 <mirkojelic.jelic@gmail.com>
2018-10-11 22:55:49 +0200

    error in mongolab coccection still again

--- mongoDB/CRUD/findById.js
+++ mongoDB/CRUD/findById.js
@@ -1,5 +1,5 @@
 const { MongoClient, ObjectID } = require('mongodb')
-const url = 'mongodb://heroku_j5rtxxh3:76a4mtii4t73tu2dq8g1s6ob4b@ds129233.mlab.com:29233/heroku_j5rtxxh3';
+const url = 'mongodb://mirko:fionfion00@ds129233.mlab.com:29233/heroku_j5rtxxh3'
 
 const findById = async (dbName,collName,id) => {
   const client = await MongoClient.connect(url,{ useNewUrlParser: true })
--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.post('/todos', asyncHandler(async (req,res) => {
 }))
 app.get('/todos', asyncHandler(async (req,res) => {
   try {
-    const allTodos = await findAll('todo', 'todos')
+    const allTodos = await findAll('heroku_j5rtxxh3', 'todos')
     res.send({Todos:allTodos})
   } catch (err) {
     res.status(400).send(`Error: ${err.message}`)
@@ -34,7 +34,7 @@ app.get('/todos', asyncHandler(async (req,res) => {
 app.get('/todos/:id', asyncHandler(async (req,res) => {
   try {
     const todoId = await req.params.id
-    const todo = await findById('todo', 'todos', todoId)
+    const todo = await findById('heroku_j5rtxxh3', 'todos', todoId)
     if(todo){
       res.send({todo})
     } else {
